b176cf26bf219c66ab8b3dd2f48dc1e8c02145b6
Haruto <ynhrt1250@gmail.com>
2018-03-17 09:43:22 +0900

    clean

--- snprintf.cpp
+++ snprintf.cpp
@@ -24,28 +24,28 @@ public:
 		for (auto &e : buffer) e = '\0';
 	}
 
-	size_t Print(const char* format, const size_t max_length) {
+	size_t Print(const char* format, const size_t max_width) {
 		if (format == nullptr) return 0;
-		return Output(format, max_length, 0, 0, Pad::Default());
+		return Output(format, max_width, 0, 0, Pad::Default());
 	}
 
 	template<typename Head, typename ... Tail>
-	size_t Print(const char* format, const size_t max_length, const Head& head, const Tail&... tail) {
+	size_t Print(const char* format, const size_t max_width, const Head& head, const Tail&... tail) {
 		if (format == nullptr) return 0;
 
-		size_t length = 0;
+		size_t printed_width = 0;
 		for (; *format != '%'; format++) {
-			if (max_length == length || *format == '\0') return length;
-			length += PrintChar(*format);
+			if (max_width == printed_width || *format == '\0') return printed_width;
+			printed_width += PrintChar(*format);
 		}
 		format++;
 		size_t width = 0;
 		Pad pad;
 
-		if (*format == '\0') return length;
+		if (*format == '\0') return printed_width;
 		if (*format == '%') {
-			length += PrintChar('%');
-			return length + Print(++format, max_length - length, head, tail...);
+			printed_width += PrintChar('%');
+			return printed_width + Print(++format, max_width - printed_width, head, tail...);
 		}
 		for (; *format == '-' || *format == '+'; format++) {
 			if (*format == '-') pad.right = false;
@@ -67,8 +67,8 @@ public:
 		}
 		else width_decimal = 6;
 
-		length += Output(head, max_length, width, width_decimal, pad);
-		return length + Print(++format, max_length - length, tail...);
+		printed_width += Output(head, max_width, width, width_decimal, pad);
+		return printed_width + Print(++format, max_width - printed_width, tail...);
 	}
 
 	void Show() {
@@ -82,36 +82,35 @@ private:
 	}
 
 	//char
-	size_t Output(const char c, const size_t max_length, const size_t width,const size_t width_decimal,const Pad pad) {
+	size_t Output(const char c, const size_t max_width, const size_t width,const size_t width_decimal,const Pad pad) {
 		const char str[2] = { c,'\0' };
-		return Output(str, max_length, width, width_decimal, pad);
+		return Output(str, max_width, width, width_decimal, pad);
 	}
 	
 	//bool
-	size_t Output(const bool b, const size_t max_length, const size_t width,const size_t width_decimal,const Pad pad) {
-		return Output(b ? "true" : "false", max_length, width, width_decimal, pad);
+	size_t Output(const bool b, const size_t max_width, const size_t width,const size_t width_decimal,const Pad pad) {
+		return Output(b ? "true" : "false", max_width, width, width_decimal, pad);
 	}
 
 	//string
-	size_t Output(const char* str, const size_t max_length, const size_t width,const size_t width_decimal,const Pad pad) {
-		const size_t length = GetLength(str);
+	size_t Output(const char* str, const size_t max_width, const size_t width,const size_t width_decimal,const Pad pad) {
 		size_t i = 0;
 		if (pad.right) {
 			//if misses the cast, underflow can happen
-			for (; (int32_t)i < (int32_t)(width - length); i++) {
-				if (i == max_length) return max_length;
+			for (; (int32_t)i < (int32_t)width - (int32_t)GetLength(str); i++) {
+				if (i == max_width) return max_width;
 				if (pad.zero) buffer[itr++] = '0';
 				else buffer[itr++] = ' ';
 			}
 		}
 		for (; *str != '\0'; str++) {
-			if (i == max_length) return max_length;
+			if (i == max_width) return max_width;
 			buffer[itr++] = *str;
 			i++;
 		}
 		if (!pad.right) {
 			for (; i < width; i++) {
-				if (i == max_length) return max_length;
+				if (i == max_width) return max_width;
 				buffer[itr++] = ' ';
 			}
 		}
@@ -119,77 +118,77 @@ private:
 	}
 
 	//int
-	size_t Output(const int32_t _n, const size_t max_length, const size_t width,const size_t width_decimal,const Pad pad) {
+	size_t Output(const int32_t _n, const size_t max_width, const size_t width,const size_t width_decimal,const Pad pad) {
 		char str[MAX_NUMBER_LENGTH];
 
-		int32_t n = Abs(_n);
-		const size_t digit = GetDigit(n);
-		size_t length_to_print = digit;
+		int32_t integer = Abs(_n);
+		const size_t width_integer = GetDigit(integer);
+		size_t width_sign = 0;
 
-		size_t printed_length = 0;
+		size_t printed_width = 0;
 		if (_n < 0) {
-			if (pad.zero && pad.right) printed_length += PrintChar('-');
+			if (pad.zero && pad.right) printed_width += PrintChar('-');
 			else {
 				str[0] = '-';
-				length_to_print++;
+				width_sign++;
 			}
 		}
 		if (_n >= 0 && pad.plus) {
-			if (pad.zero && pad.right) printed_length += PrintChar('+');
+			if (pad.zero && pad.right) printed_width += PrintChar('+');
 			else {
 				str[0] = '+';
-				length_to_print++;
+				width_sign++;
 			}
 		}
 
-		for (int32_t i = 0; i < digit ; i++) {
-			str[length_to_print - i - 1] = n % 10 + '0';
-			n /= 10;
+		for (int32_t i = 0; i < width_integer ; i++) {
+			str[width_sign + width_integer - i - 1] = integer % 10 + '0';
+			integer /= 10;
 		}
-		str[length_to_print] = '\0';
-		return printed_length + Output(str, max_length - printed_length, width - printed_length, width_decimal, pad);
+		str[width_sign + width_integer] = '\0';
+		return printed_width + Output(str, max_width - printed_width, width - printed_width, width_decimal, pad);
 	}
 
 	//float
-	size_t Output(const float _n, const size_t max_length, const size_t width,const size_t width_decimal,const Pad pad) {
+	size_t Output(const float _n, const size_t max_width, const size_t width,const size_t width_decimal,const Pad pad) {
 		char str[MAX_NUMBER_LENGTH];
 
 		int32_t integer = (int32_t)Abs(_n);
 		int32_t decimal = (Abs(_n) - (float)integer)*Pow10(width_decimal);
-		const size_t length_integer = GetDigit(integer);
-		size_t length_sign = 0;
+		const size_t width_integer = GetDigit(integer);
+		size_t width_sign = 0;
 
-		size_t printed_length = 0;
+		size_t printed_width = 0;
 		if (_n < 0) {
-			if (pad.zero && pad.right) printed_length += PrintChar('-');
+			if (pad.zero && pad.right) printed_width += PrintChar('-');
 			else {
 				str[0] = '-';
-				length_sign++;
+				width_sign++;
 			}
 		}
 		if (_n >= 0 && pad.plus) {
-			if (pad.zero && pad.right) printed_length += PrintChar('+');
+			if (pad.zero && pad.right) printed_width += PrintChar('+');
 			else {
 				str[0] = '+';
-				length_sign++;
+				width_sign++;
 			}
 		}
 
-		for (int32_t i = 0; i < length_integer ; i++) {
-			str[length_sign + length_integer - i - 1] = integer % 10 + '0';
+		for (int32_t i = 0; i < width_integer ; i++) {
+			str[width_sign + width_integer - i - 1] = integer % 10 + '0';
 			integer /= 10;
 		}
 		if(width_decimal > 0){
-			str[length_sign + length_integer] = '.';
+			str[width_sign + width_integer] = '.';
 			for (int32_t i = 0; i < width_decimal ; i++) {
-				str[length_sign + length_integer + width_decimal - i] = decimal % 10 + '0';
+				str[width_sign + width_integer + width_decimal - i] = decimal % 10 + '0';
 				decimal /= 10;
 			}	
-			str[length_sign + length_integer + width_decimal + 1] = '\0';
+			str[width_sign + width_integer + width_decimal + 1] = '\0';
 		}
-		else str[length_sign + length_integer] = '\0';
+		else str[width_sign + width_integer] = '\0';
 		
-		return printed_length + Output(str, max_length - printed_length, width - printed_length, width_decimal, pad);
+		return printed_width + Output(str, max_width - printed_width, width - printed_width, width_decimal, pad);
 	}
 
 	size_t GetLength(const char* str) {
